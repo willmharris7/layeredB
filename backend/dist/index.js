f7261bbca40c3cf4c3bafa70b78fd85ab3620620
Will Harris <willmharris7@gmail.com>
2021-10-06 16:13:41 -0700

    typescript test endpoints working

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,19 +1,66 @@
 "use strict";
+// import bodyParser from 'body-parser';
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+// app.get('/', (req: Request, res: Response) => {
+//   res.send('<h1>Hello from the TypeScript world!</h1>');
+// });
+// app.listen(PORT, () => console.log(`Running on ${PORT} ⚡`));
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// const express = require('express')
 var express_1 = __importDefault(require("express"));
-var body_parser_1 = __importDefault(require("body-parser"));
 var helmet_1 = __importDefault(require("helmet"));
 var dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
-var PORT = process.env.PORT || 3000;
+var MongoClient = require('mongodb').MongoClient;
 var app = (0, express_1.default)();
+dotenv_1.default.config();
+var PORT = process.env.PORT || 8080;
+var uri = "mongodb+srv://first_test_user:im7p9hcVdHo5aS@cluster0.1m63c.mongodb.net/test?retryWrites=true&w=majority";
+var client = new MongoClient(uri);
 app.use((0, helmet_1.default)());
-app.use(body_parser_1.default.json());
-app.use(body_parser_1.default.urlencoded({ extended: true }));
-app.get('/', function (req, res) {
-    res.send('<h1>Hello from the TypeScript world!</h1>');
+app.use(express_1.default.json()); // replaces bodyParser.json()
+app.use(express_1.default.urlencoded({ extended: true })); // for Postman: use x-www-form-urlencoded to post 
+app.listen(PORT);
+// Test endpoints //
+app.get('/api/hello', function (req, res) {
+    res.send({ express: 'Hello From Express' });
 });
-app.listen(PORT, function () { return console.log("Running on " + PORT + " \u26A1"); });
+app.post('/api/world', function (req, res) {
+    res.send("I received your POST request. This is what you sent me: " + Object.values(req.body)[0]);
+});
+// Test endpoints //
+// app.get('/api/dbs', async function (req: Request, res: Response) {
+//   await client.connect()
+//   dbList = await client.db().admin().listDatabases()
+//   dbNames = []
+//   dbList.databases.forEach(db => dbNames.push(db.name))
+//   await client.close()
+//   res.send({ express: dbNames })
+// })
+// app.get('/api/top5', async function (req: Request, res: Response) {
+//   await client.connect()
+//   const dbResp = client.db('sample_airbnb').collection('listingsAndReviews').find().limit(5)
+//   top5Listings = await dbResp.toArray()
+//   top5ListingsNamesBedrooms = []
+//   for (const listing of top5Listings) {
+//     top5ListingsNamesBedrooms.push(listing.name + " has " + listing.bedrooms + " bedrooms")
+//   }
+//   await client.close()
+//   res.send({ express: top5ListingsNamesBedrooms })
+// })
+// app.post('/api/bedrooms', async function (req: Request, res: Response) {
+//   const number = parseInt(Object.values(req.body)[0])
+//   await client.connect()
+//   const dbResp = client.db('sample_airbnb').collection('listingsAndReviews').find({"bedrooms": number}).limit(5)
+//   top5Listings = await dbResp.toArray()
+//   top5ListingsNamesBedrooms = []
+//   for (const listing of top5Listings) {
+//     top5ListingsNamesBedrooms.push(listing.name + " has " + listing.bedrooms + " bedrooms")
+//   }
+//   await client.close()
+//   res.send({ express: top5ListingsNamesBedrooms })
+// })
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,71 @@
-import express, { Express, Request, Response } from 'express';
-import bodyParser from 'body-parser';
+// import bodyParser from 'body-parser';
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.get('/', (req: Request, res: Response) => {
+//   res.send('<h1>Hello from the TypeScript world!</h1>');
+// });
+
+// app.listen(PORT, () => console.log(`Running on ${PORT} ⚡`));
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// const express = require('express')
+import express, { Express, Request, Response } from 'express'
 import helmet from 'helmet';
 import dotenv from 'dotenv';
-
-dotenv.config();
-
-const PORT = process.env.PORT || 3000;
-const app: Express = express();
-
+const {MongoClient} = require('mongodb')
+const app: Express = express()
+dotenv.config()
+const PORT = process.env.PORT || 8080
+const uri = "mongodb+srv://first_test_user:im7p9hcVdHo5aS@cluster0.1m63c.mongodb.net/test?retryWrites=true&w=majority"
+const client = new MongoClient(uri)
 app.use(helmet());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json()) // replaces bodyParser.json()
+app.use(express.urlencoded({extended: true})) // for Postman: use x-www-form-urlencoded to post 
+app.listen(PORT)
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('<h1>Hello from the TypeScript world!</h1>');
-});
+// Test endpoints //
+// req is required even for simple get paths, because express interprests the first param as Request
+app.get('/api/hello', (req: Request, res: Response) => {
+  res.send({ express: 'Hello From Express' })
+})
 
-app.listen(PORT, () => console.log(`Running on ${PORT} ⚡`));
+app.post('/api/world', (req: Request, res: Response) => {
+  res.send(`I received your POST request. This is what you sent me: ${Object.values(req.body)[0]}`)
+})
+// Test endpoints //
+
+// app.get('/api/dbs', async function (req: Request, res: Response) {
+//   await client.connect()
+//   dbList = await client.db().admin().listDatabases()
+//   dbNames = []
+//   dbList.databases.forEach(db => dbNames.push(db.name))
+//   await client.close()
+//   res.send({ express: dbNames })
+// })
+
+// app.get('/api/top5', async function (req: Request, res: Response) {
+//   await client.connect()
+//   const dbResp = client.db('sample_airbnb').collection('listingsAndReviews').find().limit(5)
+//   top5Listings = await dbResp.toArray()
+//   top5ListingsNamesBedrooms = []
+//   for (const listing of top5Listings) {
+//     top5ListingsNamesBedrooms.push(listing.name + " has " + listing.bedrooms + " bedrooms")
+//   }
+//   await client.close()
+//   res.send({ express: top5ListingsNamesBedrooms })
+// })
+
+// app.post('/api/bedrooms', async function (req: Request, res: Response) {
+//   const number = parseInt(Object.values(req.body)[0])
+//   await client.connect()
+//   const dbResp = client.db('sample_airbnb').collection('listingsAndReviews').find({"bedrooms": number}).limit(5)
+//   top5Listings = await dbResp.toArray()
+//   top5ListingsNamesBedrooms = []
+//   for (const listing of top5Listings) {
+//     top5ListingsNamesBedrooms.push(listing.name + " has " + listing.bedrooms + " bedrooms")
+//   }
+//   await client.close()
+//   res.send({ express: top5ListingsNamesBedrooms })
+// })
